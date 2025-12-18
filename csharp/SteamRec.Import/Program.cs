c1906a6516b1dde23f2602c4ab98cbeaa56dbf93
Jopsakee <102683378+Jopsakee@users.noreply.github.com>
2025-12-18 18:53:50 +0100

    Code clean + gitignore

--- csharp/SteamRec.Import/Program.cs
+++
@@ -1,6 +0,0 @@
-using SteamRec.Import;
-
-var repoRoot = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", ".."));
-var csvPath = Path.Combine(repoRoot, "data", "processed", "games_clean.csv");
-
-await ImportGamesToMongo.RunAsync(csvPath);
